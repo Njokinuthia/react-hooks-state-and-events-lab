345902d2ae1843b86ec7ec48c0bf916524afcac5
Njoki <kinuthiash@gmail.com>
2022-05-15 17:15:18 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,17 +6,30 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [darkMode, setDarkMode] = useState(false)
+
+  const btnLabel = darkMode ? "Light Mode" : "Dark Mode"
+
+  function handleDarkMode() {
+    setDarkMode((darkMode)=>!darkMode)
+  }
+
+  const appClass = darkMode ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkMode}>{btnLabel}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
   );
+
+ 
+
 }
+
+
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React , {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const[inCart, setInCart]=useState(false);
+  const listClassName = inCart? "in-cart" : "";
+  const btnClassName = inCart? "remove":"add"
+  const btnLabel = inCart? "Add To Cart" : "Remove From Cart"
+
+  function addToCartClick(){
+    setInCart(inCart =>!inCart)
+  }
+
   return (
-    <li className="">
+    <li className={listClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCartClick} className={btnClassName}>{btnLabel}</button>
     </li>
   );
 }
